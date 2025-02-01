3e22f2f7d8bbc79d02d352352d90b2086d23ea19
zyonson <wakeup5216@gmail.com>
2025-02-01 03:57:04 +0000

    use cache when renderTemplate

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -6,7 +6,12 @@ import (
 	"github.com/zyonson/go-course/pkg/render"
 )
 
-// Home is the home page handler
+/*
+Home is the home page handler
+
+	w http.ResponseWriter: This parameter is used to write the HTTP response back to the client.
+	r *http.Request: This parameter contains all the information about the HTTP request, such as headers, URL, method, and more.
+*/
 func Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.html")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,14 +3,51 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-// RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, html string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+html, "./templates/base.layout.html")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var html *template.Template
+	var err error
+
+	// check to see if we alredy have the template in our cache
+	_, inMap := tc[t]
+	if !inMap {
+		// need to create the template
+		log.Println("creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		log.Println("using cached template")
 	}
+
+	html = tc[t]
+
+	err = html.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.html",
+	}
+
+	// parse the template
+	html, err := template.ParseFiles(templates...)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// add template to cache (map)
+	tc[t] = html
+
+	return nil
 }
