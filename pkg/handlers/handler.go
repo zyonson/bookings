a996dcae6a58874df1db7f37f99aa53a2c33728e
zyonson <wakeup5216@gmail.com>
2025-02-06 14:45:24 +0000

    add IPadress using session

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -32,6 +32,8 @@ func NewHandlers(r *Repository) {
 // w http.ResponseWriter: This parameter is used to write the HTTP response back to the client.
 // r *http.Request: This parameter contains all the information about the HTTP request, such as headers, URL, method, and more.
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
@@ -40,6 +42,10 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	// send the data to the template
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
